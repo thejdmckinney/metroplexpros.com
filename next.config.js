edb77e530d4999f9d4dbd38dce2f6c0f0e7ccaed
thejdmckinney <jeremy.mckinney@creativeconstructorsllc.com>
2025-11-15 01:38:26 -0600

    Add GitHub Pages deployment with custom domain support

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  basePath: '',
+  assetPrefix: '',
   images: {
     domains: [],
+    unoptimized: true
   },
 }
 
