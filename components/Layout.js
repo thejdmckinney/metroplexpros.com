548a4285dc5396352403dd274da76c2ff00ba3c9
thejdmckinney <jeremy.mckinney@creativeconstructorsllc.com>
2025-11-15 11:06:06 -0600

    Remove visual editor and edit page button from all public pages for professional appearance

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,6 @@
-import { useState, useEffect } from 'react'
 import SEO from './SEO'
 import Header from './Header'
 import Footer from './Footer'
-import VisualEditor from './VisualEditor'
 
 export default function Layout({ 
   children, 
@@ -13,32 +11,6 @@ export default function Layout({
   openGraph,
   localBusiness = true
 }) {
-  const [isEditing, setIsEditing] = useState(false)
-
-  useEffect(() => {
-    // Check if visual editor should be active
-    const editorActive = localStorage.getItem('visualEditorActive') === 'true'
-    setIsEditing(editorActive)
-    
-    // Add editing class to body
-    if (editorActive) {
-      document.body.classList.add('editing')
-    } else {
-      document.body.classList.remove('editing')
-    }
-  }, [isEditing])
-
-  const handleEditorToggle = (active) => {
-    setIsEditing(active)
-    localStorage.setItem('visualEditorActive', active.toString())
-    
-    if (active) {
-      document.body.classList.add('editing')
-    } else {
-      document.body.classList.remove('editing')
-    }
-  }
-
   return (
     <>
       <SEO 
@@ -58,7 +30,6 @@ export default function Layout({
           {children}
         </main>
         <Footer />
-        <VisualEditor enabled={isEditing} onToggle={handleEditorToggle} />
       </div>
     </>
   )
