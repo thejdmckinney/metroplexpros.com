e7c9cca8065a9dc0832c4703ab6c648d253b4f50
thejdmckinney <jeremy.mckinney@creativeconstructorsllc.com>
2025-12-06 03:05:31 -0600

    Add tawk.to live chat widget to all pages

---
+++ pages/_document.js
@@ -0,0 +1,31 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+        
+        {/* Tawk.to Live Chat Widget */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+              (function(){
+              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+              s1.async=true;
+              s1.src='https://embed.tawk.to/6933eff1dc2f61197c66de83/1jbpe2i4c';
+              s1.charset='UTF-8';
+              s1.setAttribute('crossorigin','*');
+              s0.parentNode.insertBefore(s1,s0);
+              })();
+            `,
+          }}
+        />
+      </body>
+    </Html>
+  )
+}
